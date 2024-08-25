87a6cc7911aa16a87bcc8d929b301f0eaab34662
Marc Rufer <marc.rufer@isolutions.ch>
2024-08-25 22:58:21 +0200

    minor

--- src/ArbitraryApp/Server/Cae/WebApiMsalUiRequiredException.cs
+++ src/ArbitraryApp/Server/Cae/WebApiMsalUiRequiredException.cs
@@ -4,31 +4,23 @@ using System.Net.Http.Headers;
 namespace ArbitraryApp.Server.Cae;
 
 /// <summary>
-/// This exception class is used to pass HTTP CAE unauthorized responses from a Httpclient and 
+/// This exception class is used to pass HTTP CAE unauthorized responses from a HttpClient and 
 /// return the WWW-Authenticate header with the required claims challenge. 
 /// This is only required if using a downstream API
 /// </summary>
 public class WebApiMsalUiRequiredException : Exception
 {
-    private readonly HttpResponseMessage httpResponseMessage;
+    private readonly HttpResponseMessage _httpResponseMessage;
 
-    public WebApiMsalUiRequiredException(string message, HttpResponseMessage response) : base(message)
+    public WebApiMsalUiRequiredException(string message, HttpResponseMessage response)
+        : base(message)
     {
-        httpResponseMessage = response;
+        _httpResponseMessage = response;
     }
 
-    public HttpStatusCode StatusCode
-    {
-        get { return httpResponseMessage.StatusCode; }
-    }
+    public HttpStatusCode StatusCode => _httpResponseMessage.StatusCode;
 
-    public HttpResponseHeaders Headers
-    {
-        get { return httpResponseMessage.Headers; }
-    }
+    public HttpResponseHeaders Headers => _httpResponseMessage.Headers;
 
-    public HttpResponseMessage HttpResponseMessage
-    {
-        get { return httpResponseMessage; }
-    }
+    public HttpResponseMessage HttpResponseMessage => _httpResponseMessage;
 }
